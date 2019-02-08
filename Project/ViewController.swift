78ef298b9463a7de22ea5e4aab3ffdfae146a068
Donkemezuo <raymonddonkemezuo.org>
2019-02-08 11:14:37 -0500

    added another print statement

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("omg another change")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
